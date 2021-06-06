a98bb0a6a0a155b525c1751e569f719c3c559c62
Nessquik <asiamarcinkowska@gmail.com>
2021-06-06 10:33:11 +0200

    zaimplementowano binary_to_decimal

--- seti.py
+++ seti.py
@@ -8,7 +8,10 @@ def decimal_to_binary(decimal_number):
 
 def binary_to_decimal(binary_digits):
     """Returns the decimal (number) representation of a binary number represented by an array of 0/1 digits"""
-    pass
+    number = 0
+    for power, digit in enumerate(reversed(binary_digits)):
+        number += digit * (2 ** power)
+    return number
 
 
 def decimal_to_base(decimal_number, destination_base):
